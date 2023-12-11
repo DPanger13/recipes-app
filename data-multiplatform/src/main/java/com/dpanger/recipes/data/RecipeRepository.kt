26afd71f238dc8b9b847c83934fa5124fbfc1430
Derek Panger <12522587+DPanger13@users.noreply.github.com>
2023-12-10 21:23:54 -0600

    Create temporary multiplatform lib

--- data-multiplatform/src/main/java/com/dpanger/recipes/data/RecipeRepository.kt
+++ data-multiplatform/src/main/java/com/dpanger/recipes/data/RecipeRepository.kt
@@ -4,3 +4,12 @@ interface RecipeRepository {
     suspend fun getRecipes(): Result<List<Recipe>>
     suspend fun saveRecipe(title: String): Result<Unit>
 }
+
+fun recipeRepository() =
+    object : RecipeRepository {
+        override suspend fun getRecipes(): Result<List<Recipe>> =
+            Result.success(emptyList())
+
+        override suspend fun saveRecipe(title: String): Result<Unit> =
+            Result.success(Unit)
+    }
---
+++ data/build.gradle.kts
@@ -0,0 +1,43 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    alias(libs.plugins.com.android.library)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    kotlin("kapt")
+    alias(libs.plugins.hilt)
+}
+
+android {
+    namespace = "com.dpanger.recipes.data"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+}
+
+dependencies {
+    implementation(project(":data-multiplatform"))
+    implementation(libs.hilt)
+    kapt(libs.hilt.compiler)
+}
