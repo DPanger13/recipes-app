396a7060a54264708607a42af8679f15d1f2c2ae
Derek Panger <12522587+DPanger13@users.noreply.github.com>
2023-12-17 00:09:16 -0600

    Add convention plugin for app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,55 +1,23 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("recipes-application")
     kotlin("kapt")
     alias(libs.plugins.hilt)
 }
 
 android {
     namespace = "com.dpanger.recipes"
-    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.dpanger.recipes"
-        minSdk = 24
-        targetSdk = 34
         versionCode = 1
         versionName = "0.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlin {
-        jvmToolchain(17)
-    }
-
-    buildFeatures {
-        compose = true
-    }
-
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
-    }
-
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
