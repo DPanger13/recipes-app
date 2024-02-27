49d5ad1f8d4e70259e48652b07c25dcd20f70fb4
Derek Panger <12522587+DPanger13@users.noreply.github.com>
2024-02-26 18:08:02 -0600

    Remove add feature

--- add/src/main/java/com/dpanger/recipes/add/AddRecipeUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.dpanger.recipes.add
-
-sealed interface AddRecipeUiState {
-    data object InProgress : AddRecipeUiState
-
-    data object Saving : AddRecipeUiState
-    data object SavingFailed : AddRecipeUiState
-    data object Saved : AddRecipeUiState
-}
