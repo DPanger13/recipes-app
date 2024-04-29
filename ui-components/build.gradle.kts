79f94079b3c21a66c258a82c9d78857b278d02c1
Derek Panger <12522587+DPanger13@users.noreply.github.com>
2024-04-29 17:42:57 -0500

    Update home UI (#19)

---
+++ ui-components/build.gradle.kts
@@ -0,0 +1,16 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("library-android-compose")
+}
+
+android {
+    namespace = "com.dpanger.vehicles.uicomponents"
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.bundles.compose.debug)
+}
