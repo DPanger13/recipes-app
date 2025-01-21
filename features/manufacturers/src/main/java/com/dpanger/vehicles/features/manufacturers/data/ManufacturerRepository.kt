42d18da650db835d8ea158515bf40d17ac4a3adc
Derek Panger <12522587+DPanger13@users.noreply.github.com>
2025-01-21 17:34:53 -0600

    Switch threads in repository
    The multi-platform lib is synchronous and doesn't handle switching
    threads like Retrofit does.

--- features/manufacturers/src/main/java/com/dpanger/vehicles/features/manufacturers/data/ManufacturerRepository.kt
+++ features/manufacturers/src/main/java/com/dpanger/vehicles/features/manufacturers/data/ManufacturerRepository.kt
@@ -3,5 +3,3 @@ package com.dpanger.vehicles.features.manufacturers.data
 interface ManufacturerRepository {
     suspend fun all(): List<Manufacturer>?
 }
-
-fun manufacturerRepository(): ManufacturerRepository = ManufacturerRepositoryImpl()
