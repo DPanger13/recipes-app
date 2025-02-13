77edd11c5b494d1350c829b062fba1b9fea4d151
Derek Panger <12522587+DPanger13@users.noreply.github.com>
2025-02-13 17:07:33 -0600

    Clean up application gradle dependencies

--- build-logic/src/main/kotlin/com/dpanger/vehicles/recipes-application.gradle.kts
+++ build-logic/src/main/kotlin/com/dpanger/vehicles/recipes-application.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("com.android.application")
-    kotlin("android")
     id("org.jlleitschuh.gradle.ktlint")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.dagger.hilt.android")
+    kotlin("android")
+    kotlin("kapt")
 }
 
 android {
@@ -36,4 +38,20 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+
+    val hiltVersion = "2.49"
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-compiler:$hiltVersion")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 }
