9d4b1638c2d7a608d60c5551acd1a07965dd9840
Derek Panger <12522587+DPanger13@users.noreply.github.com>
2025-02-13 12:20:20 -0600

    Update libs and target SDK (#40)

--- build-logic/src/main/kotlin/com/dpanger/vehicles/library-android-compose.gradle.kts
+++ build-logic/src/main/kotlin/com/dpanger/vehicles/library-android-compose.gradle.kts
@@ -13,14 +13,14 @@ android {
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
 
     implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.6")
 }
